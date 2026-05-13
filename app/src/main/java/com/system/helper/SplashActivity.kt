ce90d1ed8d5707430ebf3b7090cc2881229d16f9
wingeherose9145 <121928673+wingeherose9145@users.noreply.github.com>
2026-05-13 17:36:54 +0800

    Add SplashActivity for app startup screen

---
+++ app/src/main/java/com/system/helper/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.system.helper
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            R.layout.activity_splash
+        )
+
+        Handler(
+            Looper.getMainLooper()
+        ).postDelayed({
+
+            startActivity(
+                Intent(
+                    this,
+                    FakeHomeActivity::class.java
+                )
+            )
+
+            finish()
+
+        }, 1500)
+    }
+}
